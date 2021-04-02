204f4daa2d766b8ccb879bd199dd13a10fd771d5
fight1991 <359193813@qq.com>
2021-04-02 14:33:57 +0800

    暂存

--- js/layout.js
+++ js/layout.js
@@ -3,4 +3,14 @@ $(function () {
     animateThreshold: 100,
     scrollPollInterval: 20
   });
+  var $tab = $('.features-tab-left')
+  var $tabContent = $('.features-tab-right-item')
+  $tab.click(function (e) {
+    var currLi = e.target;
+    $(currLi).addClass('current-show').siblings().removeClass("current-show");
+    var currentIndex = $(currLi).data('index');
+    if (typeof currentIndex === 'number') {
+      $tabContent.eq(currentIndex).addClass('show').siblings().removeClass('show')
+    }
+  })
 })
