f0cbc21355949b3ec63244a57393a5192abf1fe0
fight1991 <359193813@qq.com>
2021-04-07 11:24:44 +0800

    官网暂存

--- js/news.js
+++ js/news.js
@@ -1,6 +1,6 @@
 $(function() {
   $('.pagination-box').pagination({
-    dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    dataSource: [1, 2, 3, 4, 5],
     prevText: '上一页',
     nextText: '下一页',
     pageSize: 5,
