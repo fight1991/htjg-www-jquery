49467a06c15dc72d8b3a7ded671e71e0b728d375
fight1991 <359193813@qq.com>
2021-04-19 19:22:33 +0800

    暂存

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ $(function() {
 
   // 初始化swiper
   var mySwiper = new Swiper('.swiper-container', {
-    autoplay: false,//可选选项，自动滑动
+    autoplay: true,//可选选项，自动滑动
     autoHeight: true,
     slidesPerView: 4,
     spaceBetween: 20,
