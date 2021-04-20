9af66410566d0c3c2d8cb7674276b83ded9e32e7
fight1991 <359193813@qq.com>
2021-04-20 14:05:38 +0800

    暂存

--- js/layout.js
+++ js/layout.js
@@ -1,12 +1,14 @@
 $(function () {
   var $tab = $('.features-tab-left')
   var $tabContent = $('.features-tab-right-item')
+  var $tabImg = $('.features-img-item')
   $tab.click(function (e) {
     var currLi = e.target;
     $(currLi).addClass('current-show').siblings().removeClass("current-show");
     var currentIndex = $(currLi).data('index');
     if (typeof currentIndex === 'number') {
       $tabContent.eq(currentIndex).addClass('show').siblings().removeClass('show')
+      $tabImg.eq(currentIndex).addClass('show').siblings().removeClass('show')
     }
   })
 })
